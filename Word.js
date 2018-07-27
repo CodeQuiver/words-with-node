314e39db0acb8a83d13bd48f124fc8b327fa9ff3
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-07-27 19:29:06 -0400

    pseudocoding sections

--- Word.js
+++ Word.js
@@ -4,9 +4,7 @@ var Letter = require("./Letter.js");
 
 // * **Word.js**: Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
 
-//   * A function that returns a string representing the word. This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
 
-//   * A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in `Letter.js`)
 
 //==================== GLOBAL VARIABLES =================================//
 
@@ -18,13 +16,26 @@ var Letter = require("./Letter.js");
 
 // word constructor
 var Word = function (answerWord) {
-    //   * An array of `new` Letter objects representing the letters of the underlying word
+    //======== DEFINE ARRAY of `new` Letter objects representing the letters of the underlying word ===//
     answerWord = answerWord.toLowerCase(); //doing this for comparisons later
     var answerArray = Array.from(answerWord); //split into array
     for (let i = 0; i < answerArray.length; i++) {
         answerArray[i] = new Letter(answerArray[i]); //sets the spot in the answer array that contained the letter to now contain the entire object representing the letter    
     }
     this.answerArray = answerArray; //finally assigns the finished new array to the new object with the key "answerArray"
+    //============= END DEFINE ARRAY ===================================================================//
+
+    //=================== METHOD 1 =====================================================================//
+    //   * A function that returns a string representing the word. This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
+
+    //=================== END METHOD 1 ==================================================================//
+
+
+    //=================== METHOD 2 ======================================================================//
+    //   * A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in `Letter.js`)
+    
+    //=================== END METHOD 2 ==================================================================//
+
 }
 
 // console.log(new Word("Awesome"));
