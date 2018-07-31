6f6d0f44eba01e9519fff76b6c3c1b0fe175b8f1
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-07-31 17:10:15 -0400

    set up method 1 buildWordString in Word file constructor but haven't yet tested

--- Letter.js
+++ Letter.js
@@ -1,11 +1,10 @@
 // * **Letter.js**: Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter.
 
 
-    //initializing constructor variables with defaults rather than having parameters passed in- will have them re-set by later logic.
 var Letter = function(newChar) {
     this.character = newChar; //   * A string value to store the underlying character for the letter
     this.guessed = false; //   * A boolean value that stores whether that letter has been guessed yet
-    this.getChar = function () {
+    this.getChar = function() {
         if (this.guessed == true) {
             return this.character;
         } else {
@@ -13,7 +12,8 @@ var Letter = function(newChar) {
         }
     } //   * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
 
-    this.checkCharGuess = function (charGuess) {
+    this.checkCharGuess = function(charGuess) {
+        // console.log("Letter.checkCharGuess: Start of function. charGuess: "+ charGuess);
         if (this.character == charGuess) {
             this.guessed = true;
             // return true; //true= the guess was a match
@@ -26,7 +26,9 @@ var Letter = function(newChar) {
     } //   * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
 }
 
+//TESTING SECTION
 // var testing = new Letter("a");
-// console.log(testing); //ran this for testing- successfully passed, but leaving this in for future tests if needed
+// console.log("This is the result of the Letter constructor: " )
+// console.log(testing);
 
 module.exports = Letter;
--- Word.js
+++ Word.js
@@ -2,41 +2,69 @@ var Letter = require("./Letter.js");
 
 //console.log(new Letter("a"));//used to check if Letter was required correctly- passed, but leaving for future ref if I change things
 
-// * **Word.js**: Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
-
+// * **Word.js**: Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess.
 
 
 //==================== GLOBAL VARIABLES =================================//
 
-// var userGuessChar = ""; //init user guess variable, for a single character
-// userGuessChar = userGuessChar.toLowerCase(); //doing this for comparisons later
+var userGuessChar = ""; //init user guess variable, for a single character
+
 
 //==================== END GLOBAL VARIABLES =================================//
 
 
 // word constructor
-var Word = function (answerWord) {
+var Word = function(answerWord) {
     //======== DEFINE ARRAY of `new` Letter objects representing the letters of the underlying word ===//
     answerWord = answerWord.toLowerCase(); //doing this for comparisons later
     var answerArray = Array.from(answerWord); //split into array
     for (let i = 0; i < answerArray.length; i++) {
         answerArray[i] = new Letter(answerArray[i]); //sets the spot in the answer array that contained the letter to now contain the entire object representing the letter    
     }
-    this.answerArray = answerArray; //finally assigns the finished new array to the new object with the key "answerArray"
+    this.answerArray = answerArray; //finally assigns the finished new array of objects to the new object with the key "answerArray"
     //============= END DEFINE ARRAY ===================================================================//
 
-    //=================== METHOD 1 =====================================================================//
-    //   * A function that returns a string representing the word. This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
+        //=================== METHOD 1 =====================================================================//
+        //   * A function that returns a string representing the word. This should call the function on each letter object ( getChar() in `Letter.js`) that displays the character or an underscore and concatenate those together.
 
-    //=================== END METHOD 1 ==================================================================//
+        this.buildWordString = function() {
+            //init wordString to build on
+            var wordString = "";
+            //loop through each Letter object in answerArray
+            for (let i = 0; i < answerArray.length; i++) {
+                // console.log("Word.buildWordString: About to call getChar for this.answerArray ele "+ i);
+                var stringResult = this.answerArray[i].getChar(); //call getChar
+                wordString = wordString + stringResult; //concatenate all returns together
+            }
+            return wordString;
+        }
+
+        //=================== END METHOD 1 ==================================================================//
 
 
-    //=================== METHOD 2 ======================================================================//
-    //   * A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in `Letter.js`)
-    
-    //=================== END METHOD 2 ==================================================================//
+        //=================== METHOD 2 ======================================================================//
+        //   * A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in `Letter.js`)
+        this.compareGuess = function(userGuessChar) {
+            userGuessChar = userGuessChar.toLowerCase(); //doing this for comparisons later
+            // console.log("Word.compareGuess: answerArray.length =" + answerArray.length);
+            for (let i = 0; i < answerArray.length; i++) {
+                // console.log("Word.compareGuess: About to call checkCharGuess for this.answerArray ele "+ i);
+                this.answerArray[i].checkCharGuess(userGuessChar);
+            }
+        }
+
+        //=================== END METHOD 2 ==================================================================//
 
 }
 
+//TEST FUNCTION
+function testWord() {
+    console.log("I'm going to test word.js");
+    var newWord = new Word("Awesome");
+    newWord.compareGuess("A");
+}
+//END TEST FUNCTION
+
 // console.log(new Word("Awesome"));
 
+// testWord();
